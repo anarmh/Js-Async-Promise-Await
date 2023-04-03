e0a1bf0133d40765bccc56381baca4454a31aba3
anarmh <anarmh@code.edu.az>
2023-04-03 10:38:13 -0700

    add Promise example

--- Practice/assets/js/script.js
+++ Practice/assets/js/script.js
@@ -21,22 +21,61 @@ let students=[
     },
     
 ]
-getStudentsByFilter("E")
+// getStudentsByFilter("E")
 
-getStudents()
+
+//  function getStudentsByFilter(str){
+//     setTimeout(() => {
+        
+//         let result=students.filter(m=>m.name.includes(str))
+//         console.log(result);
+//     }, 2000);
+//  }
+ 
+
 
 function getStudents(){
     setTimeout(() => {
-        console.log(students);
+        let str="";
+       students.forEach(stu => {
+        
+        str+=`<li class="list-group-item">
+        <span>${stu.name}-</span>
+        <span>${stu.surname}</span>
+        <span>${stu.age}</span>
+        </li> `
+        document.querySelector("ul").innerHTML=str;
+       });
     }, 3000);
     
 }
 
- function getStudentsByFilter(str){
-    setTimeout(() => {
-        
-        let result=students.filter(m=>m.name.includes(str))
-        console.log(result);
-    }, 2000);
- }
- 
+
+function createStudent(student){
+
+        return new Promise((resolved,rejected)=>{
+            setTimeout(() => {
+                students.push(student);
+                
+                let err=true;
+
+                if(!err){
+                    resolved(students);
+                }
+                else{
+                    rejected("something is wrong");
+                }
+
+            }, 2000);
+        })
+  
+}
+createStudent({name:"Atilla",surname:"Rehimov",age:19}).then(response=> {
+    
+    console.log(response);
+   }).catch(err=>{
+    console.log(err);
+   })
+  
+   
+  
