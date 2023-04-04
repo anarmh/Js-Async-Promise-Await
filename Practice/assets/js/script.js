6710670511956f38e864cc8ec874af2ca50dfa22
anarmh <anarmh@code.edu.az>
2023-04-04 03:10:32 -0700

    add practice

--- Practice/assets/js/script.js
+++ Practice/assets/js/script.js
@@ -1,26 +1,26 @@
 "use strict";
 
-let students=[
-    {
-        name:"Anar",
-        surname:"Huseynov",
-        age:36
+// let students=[
+//     {
+//         name:"Anar",
+//         surname:"Huseynov",
+//         age:36
 
-    },
-    {
-        name:"Elnar",
-        surname:"Huseynli",
-        age:25
+//     },
+//     {
+//         name:"Elnar",
+//         surname:"Huseynli",
+//         age:25
 
-    },
-    {
-        name:"Tunar",
-        surname:"Huseynli",
-        age:26
+//     },
+//     {
+//         name:"Tunar",
+//         surname:"Huseynli",
+//         age:26
 
-    },
+//     },
     
-]
+// ]
 // getStudentsByFilter("E")
 
 
@@ -34,48 +34,125 @@ let students=[
  
 
 
-function getStudents(){
-    setTimeout(() => {
-        let str="";
-       students.forEach(stu => {
+// function getStudents(){
+//     setTimeout(() => {
+//         let str="";
+//        students.forEach(stu => {
         
-        str+=`<li class="list-group-item">
-        <span>${stu.name}-</span>
-        <span>${stu.surname}</span>
-        <span>${stu.age}</span>
-        </li> `
-        document.querySelector("ul").innerHTML=str;
-       });
-    }, 3000);
+//         str+=`<li class="list-group-item">
+//         <span>${stu.name}-</span>
+//         <span>${stu.surname}</span>
+//         <span>${stu.age}</span>
+//         </li> `
+//         document.querySelector("ul").innerHTML=str;
+//        });
+//     }, 3000);
     
-}
+// }
 
 
-function createStudent(student){
+// function createStudent(student){
 
-        return new Promise((resolved,rejected)=>{
-            setTimeout(() => {
-                students.push(student);
+//         return new Promise((resolved,rejected)=>{
+//             setTimeout(() => {
+//                 students.push(student);
                 
-                let err=true;
+//                 let err=true;
 
-                if(!err){
-                    resolved(students);
-                }
-                else{
-                    rejected("something is wrong");
-                }
+//                 if(!err){
+//                     resolved(students);
+//                 }
+//                 else{
+//                     rejected("something is wrong");
+//                 }
 
-            }, 2000);
-        })
+//             }, 2000);
+//         })
   
-}
-createStudent({name:"Atilla",surname:"Rehimov",age:19}).then(response=> {
+// }
+// createStudent({name:"Atilla",surname:"Rehimov",age:19}).then(response=> {
     
-    console.log(response);
-   }).catch(err=>{
-    console.log(err);
-   })
+//     console.log(response);
+//    }).catch(err=>{
+//     console.log(err);
+//    })
   
+  
+
+
+    //   function getAllPosts(){
+
+    //     fetch('https://jsonplaceholder.typicode.com/comments')
+    //     .then(response => response.json())
+    //     .then(posts =>{
+    //         let str="";
+    //         posts.forEach(post => {
+               
+    //             str+=`
+    //             <tr data-id="${post.id}" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    //             <td>${post.id}</td>
+    //             <td>${post.postId}</td>
+    //             <td>${post.name}</td>
+    //             <td>${post.email}</td>
+    //             <td>${post.body}</td>
+    //           </tr>`
+
+    //         });
+    //         document.querySelector("tbody").innerHTML=str
+
+    //     })
+  
+    //   }
    
+    //  getAllPosts()
+
+   
+    //  setTimeout(() => {
+    //     let elems=document.querySelectorAll("tbody tr");
+    //     elems.forEach(elem => {
+    //         elem.addEventListener("click",function(){
+    //             let id=parseInt(this.getAttribute("data-id"));
+    //             fetch(`https://jsonplaceholder.typicode.com/comments/${id}`)
+    //              .then(response => response.json())
+    //             .then(json => {
+
+    //                 document.querySelector(".modal-header h5").innerText=json.name;
+    //                 document.querySelector(".modal-body").innerText=json.body;
+                
+    //             })
+           
+               
+    //         })
+    //     });
+    //  }, 2000);
+
+
+    // async function getPosts(){
+
+    //     let data=await fetch(`https://jsonplaceholder.typicode.com/comments`);
+    //     let response=await data.json()
+    //     console.log(response);
+    // }
   
+    // getPosts()
+
+
+
+    // $(function(){
+    //     $.ajax({url: "https://jsonplaceholder.typicode.com/comments", success: function(result){
+    //         console.log(result);
+    //        }});
+    // })
+
+    let btn=document.querySelector(".btn-primary")
+    
+    btn.addEventListener("click",function(){
+        fetch(`https://jsonplaceholder.typicode.com/comments/`)
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+        })
+
+    })
+      
+    
